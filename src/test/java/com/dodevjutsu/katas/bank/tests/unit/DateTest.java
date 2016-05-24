6a283ed6f84926f04b3daad65c11b51cd7cc706b
trikitrok <trikitrok@gmail.com>
2016-05-25 01:08:54 +0200

    remove duplication

--- src/test/java/com/dodevjutsu/katas/bank/tests/unit/DateTest.java
+++ src/test/java/com/dodevjutsu/katas/bank/tests/unit/DateTest.java
@@ -1,6 +1,7 @@
 package com.dodevjutsu.katas.bank.tests.unit;
 
 import com.dodevjutsu.katas.bank.Date;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,24 +9,25 @@ import static org.hamcrest.Matchers.is;
 
 public class DateTest {
 
+    private Date date;
+
+    @Before
+    public void setUp() {
+        date = new Date("07/08/2009");
+    }
+
     @Test
     public void provides_the_day() {
-        Date date = new Date("07/08/2009");
-
         assertThat(date.day(), is("07"));
     }
 
     @Test
     public void provides_the_month() {
-        Date date = new Date("07/08/2009");
-
         assertThat(date.month(), is("08"));
     }
 
     @Test
     public void provides_the_year() {
-        Date date = new Date("07/08/2009");
-
         assertThat(date.year(), is("2009"));
     }
 }
