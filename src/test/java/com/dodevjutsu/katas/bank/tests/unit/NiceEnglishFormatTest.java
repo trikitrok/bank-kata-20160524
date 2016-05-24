4bacf6e2042f966729f0bffd7ac45267ead1a34d
trikitrok <trikitrok@gmail.com>
2016-05-25 01:11:00 +0200

    remove duplication

--- src/test/java/com/dodevjutsu/katas/bank/tests/unit/NiceEnglishFormatTest.java
+++ src/test/java/com/dodevjutsu/katas/bank/tests/unit/NiceEnglishFormatTest.java
@@ -4,34 +4,38 @@ import com.dodevjutsu.katas.bank.Date;
 import com.dodevjutsu.katas.bank.Format;
 import com.dodevjutsu.katas.bank.NiceEnglishFormat;
 import com.dodevjutsu.katas.bank.StatementLine;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class NiceEnglishFormatTest {
+    Format format;
+
+    @Before
+    public void setUp() {
+        format = new NiceEnglishFormat();
+    }
+
     @Test
     public void provides_a_formatted_header() {
-        Format format = new NiceEnglishFormat();
-
         assertThat(format.header(), is("date || credit || debit || balance"));
     }
 
     @Test
     public void formats_a_credit_statement_line() {
-        Format format = new NiceEnglishFormat();
-
         assertThat(
             format.formatLine(new StatementLine(new Date("17/01/2016"), 500, 1500)),
-            is("17/01/2016 || 500.00 || || 1500.00"));
+            is("17/01/2016 || 500.00 || || 1500.00")
+        );
     }
 
     @Test
     public void formats_a_debit_statement_line() {
-        Format format = new NiceEnglishFormat();
-
         assertThat(
             format.formatLine(new StatementLine(new Date("17/01/2016"), -500, 1500)),
-            is("17/01/2016 || || 500.00 || 1500.00"));
+            is("17/01/2016 || || 500.00 || 1500.00")
+        );
     }
 }
